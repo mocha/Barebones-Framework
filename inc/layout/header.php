9b26e535220b04839ac8813ddf168e50ec484cb9
Patrick Deuley <mocha0range@gmail.com>
2010-12-03 11:04:26 -0600

    removed more boilerplate files, edited header.php

--- inc/layout/header.php
+++ inc/layout/header.php
@@ -14,8 +14,8 @@
   <meta name="author" content="">
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="/favicon.ico">
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
 
   <link rel="stylesheet" href="css/base.css?v=2">
   <link rel="stylesheet" href="css/style.css?v=2">

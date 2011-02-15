604877b885c0f2a777a84abd8944e77d12b42e5e
Ben Batschelet <bbatsche@listfeeder.net>
2011-02-15 14:05:26 -0600

    1) Moved site user content to content directory 2) Moved config to inc directory 3) Updated URL matching to allow any depth of nested folders/categories 4) Updated page title generation to convert _ to ' '.

---
+++ inc/footer.php
@@ -0,0 +1,21 @@
+        </div> <!-- end of #main -->
+        
+        <footer>
+            
+        </footer>
+    </div> <!-- end of #container -->
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.js"></script>
+    <script>!window.jQuery && document.write(unescape('%3Cscript src="<?= $baseURL ?>/js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
+    
+    <!-- scripts concatenated and minified via ant build script-->
+        <script src="<?= $baseURL ?>/js/plugins.js"></script>
+        <script src="<?= $baseURL ?>/js/script.js"></script>
+    <!-- end concatenated and minified scripts-->
+    
+    <!--[if lt IE 7 ]>
+        <script src="<?= $baseURL ?>/js/libs/dd_belatedpng.js"></script>
+        <script> DD_belatedPNG.fix('img, .png_bg'); </script>
+    <![endif]-->
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,36 @@
+<!doctype html>  
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
+    <title><?php echo $pageTitle; ?></title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?= $baseURL ?>/images/favicon.ico">
+    <link rel="apple-touch-icon" href="<?= $baseURL ?>/images/apple-touch-icon.png">
+    
+    <link rel="stylesheet" href="<?= $baseURL ?>/css/base.css?v=2">
+    <link rel="stylesheet" href="<?= $baseURL ?>/css/style.css?v=2">
+    <script src="<?= $baseURL ?>/js/libs/modernizr-1.6.min.js"></script>
+</head>
+<body>
+    <div id="container">
+        <header>
+			<h1>MyWebSite.com</h1>
+			
+			<ul id="menu">
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About Us</a></li>
+				<li><a href="/contact">Contact Us</a></li>
+			</ul>
+        </header>
+        
+        <div id="main">
+            
